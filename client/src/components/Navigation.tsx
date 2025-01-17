2fcbfe35205b11c90439482d0fd4fddcd97fcab4
LucasEdwa <lucas.eduardo2070@gmail.com>
2025-01-17 11:27:22 +0100

    navbar mobile first uppdate -incomplete

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -7,11 +7,14 @@ import logo from "../assets/logo.png";
 import { categories } from '../models/Categories';
 import CategoryDropdown from './CategoryDropdown';
 import { products } from '../data/products';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 
 export default function Navigation() {
     const categoriesRef = useRef<HTMLUListElement>(null);
     const [activeCategory, setActiveCategory] = useState<string | null>(null);
     const [hoveredCategory, setHoveredCategory] = useState<string | null>(null);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const handleCategoryClick = (category: string) => {
         setActiveCategory(prevCategory => (prevCategory === category ? null : category));
@@ -26,18 +29,28 @@ export default function Navigation() {
     };
 
     return (
-        <nav className="flex justify-center flex-col pl-6 pr-6 items-center shadow-bottom">
-            <div className="flex items-center w-full justify-between">
-                <Link className="ml-6 w-[11.2rem] h-[4.6rem]" to="/">
+        <nav className="flex lg:justify-center flex-col lg:pl-6 lg:pr-6 items-center shadow-bottom">
+            <div className="flex items-center w-full justify-between px-4">
+               <div className="lg:hidden gap-2 flex items-center justify-between ">
+               <button className=" text-md p-2" onClick={() => setMenuOpen(!menuOpen)}>
+                    <FontAwesomeIcon icon={faBars} className="text-2xl" />
+                </button>
+                <button className=" text-3xl">
+                <FontAwesomeIcon icon={faEnvelope} />
+                </button>
+                </div>
+                <Link className="lg:ml-6 lg:w-[11.2rem] lg:h-[4.6rem] w-1/4" to="/">
                     <img className="w-full h-full object-cover" src={logo} alt="logo" />
                 </Link>
-                <div className="flex items-center w-[40rem] ml-[20rem] justify-center border border-black relative">
-                    <FontAwesomeIcon icon={faFire} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-red-400" />
-                    <input className="w-full p-3 pl-10" type="text" placeholder="" />
-                    <button className="bg-black text-white p-3 text-lg"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                <div className="lg:flex lg:items-center lg:w-[40rem]  lg:ml-[20rem] lg:justify-center hidden  border border-black relative">
+                    <FontAwesomeIcon icon={faFire} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-red-400 hidden lg:block" />
+                    <input className="lg:w-full p-3 lg:pl-10  hidden  lg:block" type="text" placeholder="" />
+                    <button className="lg:bg-black lg:text-white p-3 right-0 text-lg"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
                 </div>
-                <ul className="flex gap-4 items-center w-1/4 justify-end mr-6 p-4 text-2xl text-gray-600">
-                    <li>
+                <ul className="flex gap-4 items-center w-1/4 justify-end lg:mr-6 lg:p-4 text-2xl text-gray-600">
+                    <button className="lg:hidden block text-black p-3 right-0 text-lg"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+
+                    <li className='hidden lg:block'>
                         <Link to="/user">
                             <FontAwesomeIcon icon={faUser} />
                         </Link>
@@ -52,12 +65,12 @@ export default function Navigation() {
                             <FontAwesomeIcon icon={faCartShopping} />
                         </Link>
                     </li>
-                    <li>
+                    <li className='hidden lg:block'>
                         <Link to="/support">
                             <FontAwesomeIcon icon={faHeadset} />
                         </Link>
                     </li>
-                    <li>
+                    <li className='hidden lg:block'>
                         <Link to="/language">
                             <FontAwesomeIcon icon={faGlobe} />
                         </Link>
@@ -65,12 +78,12 @@ export default function Navigation() {
                 </ul>
             </div>
             <div className="flex items-center w-full " >
-               
-                <ul ref={categoriesRef} className="flex gap-4 items-center w-full mr-6 px-2 text-sm text-black overflow-x-auto whitespace-nowrap scrollbar-hide">
+
+                <ul ref={categoriesRef} className="flex gap-4 items-center w-full mr-6 px-2 text-sm text-black overflow-x-auto whitespace-nowrap lg:scrollbar-hide ">
                     <li className="flex items-center w-fit">
-                        <Link to="/" className="w-fit text-center flex gap-1 hover:bg-gray-200 p-2 items-center group">
+                        <Link to="/" className="w-fit text-center   flex gap-1 hover:bg-gray-200 p-2 items-center group">
                             Categories
-                            <FontAwesomeIcon icon={faChevronDown} className="group-hover:animate-spinUp" />
+                            <FontAwesomeIcon icon={faChevronDown} className="group-hover:animate-spinUp hidden lg:block" />
                         </Link>
                     </li>
                     {Object.keys(categories.categories).map((category, index) => (
@@ -82,19 +95,19 @@ export default function Navigation() {
                                 tabIndex={0} role="button">
                                 {category}
                             </Link>
-                          
+
                         </li>
                     ))}
                 </ul>
-                <button onClick={scrollLeft} className="p-2">
+                <button onClick={scrollLeft} className="p-2 hidden lg:block">
                     <FontAwesomeIcon icon={faChevronLeft} />
                 </button>
-                <button onClick={scrollRight} className="p-2">
+                <button onClick={scrollRight} className="p-2 hidden lg:block">
                     <FontAwesomeIcon icon={faChevronRight} />
                 </button>
             </div>
             <div className="relative bg-white shadow-lg w-full z-50 ">
-               {hoveredCategory && (
+                {hoveredCategory && (
                     <CategoryDropdown
                         category={hoveredCategory}
                         subcategories={categories.categories[hoveredCategory]}
@@ -103,7 +116,7 @@ export default function Navigation() {
                         onMouseLeave={() => setHoveredCategory(null)}
                     />
                 )}
-                </div>
+            </div>
         </nav>
     );
 }
